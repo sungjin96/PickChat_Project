08cab87221f4fe1611be1ccbdcaebc0ed6ddf45f
taeyong <taeyong03188@gmail.com>
2019-07-08 18:34:36 +0900

    0708

---
+++ react-material-dashboard-master/src/views/Main/components/MainComponent/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+
+const MainComponent = () => {
+    return(
+        <Container>main</Container>
+    );
+}
+
+export default MainComponent
