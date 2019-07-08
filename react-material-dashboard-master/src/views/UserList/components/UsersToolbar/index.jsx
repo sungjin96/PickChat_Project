4bcd80f1fea011660ea2c4ffdd408137102bb471
taeyong <taeyong03188@gmail.com>
2019-07-08 09:40:54 +0900

    Revert "faq db 태용"
    This reverts commit 0188d3733e375f04eae22e40069e132785b5e408.

---
+++ react-material-dashboard-master/src/views/UserList/components/UsersToolbar/index.jsx
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+
+// Externals
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+// Material helpers
+import { withStyles } from '@material-ui/core';
+
+// Material components
+import { Button, IconButton } from '@material-ui/core';
+
+// Material icons
+import {
+  ArrowDownward as ArrowDownwardIcon,
+  ArrowUpward as ArrowUpwardIcon,
+  Delete as DeleteIcon
+} from '@material-ui/icons';
+
+// Shared components
+import { DisplayMode, SearchInput } from 'components';
+
+// Component styles
+import styles from './styles';
+
+class UsersToolbar extends Component {
+  render() {
+    const { classes, className, selectedUsers } = this.props;
+
+    const rootClassName = classNames(classes.root, className);
+
+    return (
+      <div className={rootClassName}>
+        <div className={classes.row}>
+          <span className={classes.spacer} />
+          {selectedUsers.length > 0 && (
+            <IconButton
+              className={classes.deleteButton}
+              onClick={this.handleDeleteUsers}>
+              <DeleteIcon />
+            </IconButton>
+          )}
+          <Button
+            className={classes.importButton}
+            size="small"
+            variant="outlined">
+            수정
+          </Button>
+          <Button
+            className={classes.exportButton}
+            size="small"
+            variant="outlined">
+            삭제
+          </Button>
+          <Button color="primary" size="small" variant="outlined">
+            생성
+          </Button>
+        </div>
+        <div className={classes.row}>
+          <SearchInput
+            className={classes.searchInput}
+            placeholder="Search user"
+          />
+          <span className={classes.spacer} />
+        </div>
+      </div>
+    );
+  }
+}
+
+UsersToolbar.propTypes = {
+  className: PropTypes.string,
+  classes: PropTypes.object.isRequired,
+  selectedUsers: PropTypes.array
+};
+
+UsersToolbar.defaultProps = {
+  selectedUsers: []
+};
+
+export default withStyles(styles)(UsersToolbar);
