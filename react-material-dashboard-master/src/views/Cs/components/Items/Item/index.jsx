0cde4fed09ef5d02f8546a3cafb77052cd137a54
sungjin5891 <sungjin5891@gmail.com>
2019-07-08 11:01:42 +0900

    다시 또 또 커밋

---
+++ react-material-dashboard-master/src/views/Cs/components/Items/Item/index.jsx
@@ -0,0 +1,100 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%'
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    flexBasis: '33.33%',
+    flexShrink: 0
+  },
+  secondaryHeading: {
+    fontSize: theme.typography.pxToRem(15),
+    color: theme.palette.text.secondary
+  }
+}));
+
+const Item = ({ title, content, comment, writer }) => {
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleChange = panel => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+  return (
+    <ExpansionPanel
+      expanded={expanded === 'panel1'}
+      onChange={handleChange('panel1')}>
+      <ExpansionPanelSummary
+        expandIcon={<ExpandMoreIcon />}
+        aria-controls="panel1bh-content"
+        id="panel1bh-header">
+        <Typography className={classes.heading}>
+          {writer === undefined ? title : writer}
+        </Typography>
+        <Typography className={classes.secondaryHeading}>
+          {writer === undefined
+            ? comment === undefined
+              ? ''
+              : content
+            : title}
+        </Typography>
+      </ExpansionPanelSummary>
+      <ExpansionPanelDetails style={{ width: '100%' }}>
+        <Typography
+          style={{ width: '100%', height: '100%', textAlign: 'center' }}>
+          <div
+            style={{
+              backgroundColor: '#F3E8FF',
+              paddingTop: '0.1rem'
+            }}>
+            {writer === undefined ? (
+              ''
+            ) : (
+              <div
+                style={{
+                  fontSize: '1.5rem',
+                  // width: '100%',
+                  textAlign: 'left',
+                  margin: '2rem',
+                  width: '95%',
+                  height: '95%',
+                  backgroundColor: '#fff'
+                }}
+              />
+            )}
+
+            {writer === undefined ? '' : content}
+          </div>
+          <div
+            style={{
+              backgroundColor: '#f8f8f8',
+              paddingTop: '0.1rem',
+              marginTop: '0.5rem'
+            }}>
+            <div
+              style={{
+                fontSize: '1.5rem',
+                width: '100%',
+                textAlign: 'left',
+                margin: '2rem'
+              }}>
+              답글
+            </div>
+            <br />
+            {comment === undefined ? content : comment}
+          </div>
+        </Typography>
+      </ExpansionPanelDetails>
+    </ExpansionPanel>
+  );
+};
+
+export default Item;
