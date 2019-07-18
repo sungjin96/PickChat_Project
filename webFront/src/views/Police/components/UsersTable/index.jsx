2ef671bd90d2b899c3215f514f8420e1b82757a8
sungjin5891 <sungjin5891@gmail.com>
2019-07-18 17:22:30 +0900

    완성

--- webFront/src/views/Police/components/UsersTable/index.jsx
+++ webFront/src/views/Police/components/UsersTable/index.jsx
@@ -15,7 +15,7 @@ import DeleteBtn from '../../../../containers/NoticeModalContainer/DeleteBtn';
 import { Portlet, PortletContent } from 'components';
 import styles from './styles';
 import Axios from 'axios';
-import {useDispatch} from 'react-redux'
+import { useDispatch } from 'react-redux';
 
 const UsersTable = ({ classes, className }) => {
   const [state, setState] = React.useState({
@@ -25,90 +25,111 @@ const UsersTable = ({ classes, className }) => {
     client: []
   });
 
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   React.useEffect(() => {
-    Axios.get('http://sungjin5891.cafe24.com/boardpolice/list').then(data =>
-    setState((state.client = data.data))
-  );
-  })
-
-    const rootClassName = classNames(classes.root, className);
-    return (
-      <Portlet className={rootClassName}>
-        <PortletContent noPadding>
-          <PerfectScrollbar>
-            <Table>
-              <TableHead>
-                <TableRow>                 
-                  <TableCell align="left" style={{ fontSize: '1.1rem' }}>번호</TableCell>
-                <TableCell align="left" style={{ fontSize: '1.1rem' }}>신고자</TableCell>
-                <TableCell align="left" style={{ fontSize: '1.1rem' }}>신고 글</TableCell>
-                <TableCell align="left" style={{ fontSize: '1.1rem' }}>신고 내용</TableCell>
-                <TableCell align="left" style={{ fontSize: '1.1rem' }}>신고 유형</TableCell>
-                <TableCell align="left" style={{ fontSize: '1.1rem' }}>삭제</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {state.client !== undefined
-                  ? state.client.map(user => (
-                      <TableRow
-                        className={classes.tableRow}
-                        hover
-                        key={user.bpno}
-                        >
-                        <TableCell className={classes.tableCell}>
-                          <div className={classes.tableCellInner}>
-                            <Link to="#">
-                              <Typography
-                                className={classes.nameText}
-                                variant="body1"
-                                style={{ fontSize: '1rem' }}>
-                                {user.bpno}
-                              </Typography>
-                            </Link>
-                          </div>
-                        </TableCell>
-                        <TableCell
-                          className={classes.tableCell}
-                          style={{ fontSize: '1rem' }}>
-                          {user.sender}
-                        </TableCell>
-                        <TableCell
-                          className={classes.tableCell}
-                          style={{ fontSize: '1rem' }}>
-                          {user.bno}
-                        </TableCell>
-
-                        <TableCell
-                          className={classes.tableCell}
-                          style={{ fontSize: '1rem' }}>
-                          {user.pcontent}
-                        </TableCell>
-                        <TableCell
-                          className={classes.tableCell}
-                          style={{ fontSize: '1rem' }}>
-                          {user.reasonid}
-                        </TableCell>
-                        <TableCell
-                      className={classes.tableCell}
-                      style={{ fontSize: '1rem' }}>
-                        <Button onClick={() => dispatch({type:'Police/DELETE', payload: {
-                          bpno: user.bpno
-                        }})}>DELETE</Button>
-                    </TableCell>
-                      </TableRow>
-                    ))
-                  : ''}
-              </TableBody>
-            </Table>
-          </PerfectScrollbar>
-        </PortletContent>
-      </Portlet>
+    Axios.get('http://sungjin5891.cafe24.com/board/police/list').then(data =>
+      setState({ client: data.data })
     );
-  }
+  });
 
+  const rootClassName = classNames(classes.root, className);
+  return (
+    <Portlet className={rootClassName}>
+      <PortletContent noPadding>
+        <PerfectScrollbar>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell align="left" style={{ fontSize: '1.1rem' }}>
+                  번호
+                </TableCell>
+                <TableCell align="left" style={{ fontSize: '1.1rem' }}>
+                  신고자
+                </TableCell>
+                <TableCell align="left" style={{ fontSize: '1.1rem' }}>
+                  신고 글
+                </TableCell>
+                <TableCell align="left" style={{ fontSize: '1.1rem' }}>
+                  신고 내용
+                </TableCell>
+                <TableCell align="left" style={{ fontSize: '1.1rem' }}>
+                  신고 유형
+                </TableCell>
+                <TableCell align="left" style={{ fontSize: '1.1rem' }}>
+                  삭제
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {state.client !== undefined
+                ? state.client.map(user => (
+                    <TableRow
+                      className={classes.tableRow}
+                      hover
+                      key={user.bpno}>
+                      <TableCell className={classes.tableCell}>
+                        <div className={classes.tableCellInner}>
+                          <Link to="#">
+                            <Typography
+                              className={classes.nameText}
+                              variant="body1"
+                              style={{ fontSize: '1rem' }}>
+                              {user.bpno}
+                            </Typography>
+                          </Link>
+                        </div>
+                      </TableCell>
+                      <TableCell
+                        className={classes.tableCell}
+                        style={{ fontSize: '1rem' }}>
+                        {user.sender}
+                      </TableCell>
+                      <TableCell
+                        className={classes.tableCell}
+                        style={{ fontSize: '1rem' }}>
+                        {user.bno}
+                      </TableCell>
+                      <TableCell
+                        className={classes.tableCell}
+                        style={{ fontSize: '1rem' }}>
+                        {user.pcontent}
+                      </TableCell>
 
-
+                      <TableCell
+                        className={classes.tableCell}
+                        style={{ fontSize: '1rem' }}>
+                        {user.rpcontent}
+                      </TableCell>
+                      <TableCell
+                        className={classes.tableCell}
+                        style={{ fontSize: '1rem' }}>
+                        {user.reasonid}
+                      </TableCell>
+                      <TableCell
+                        className={classes.tableCell}
+                        style={{ fontSize: '1rem' }}>
+                        <Button
+                          onClick={() =>
+                            dispatch({
+                              type: 'Police/DELETE',
+                              payload: {
+                                bpno: user.bpno
+                              }
+                            })
+                          }>
+                          DELETE
+                        </Button>
+                      </TableCell>
+                    </TableRow>
+                  ))
+                : ''}
+            </TableBody>
+          </Table>
+        </PerfectScrollbar>
+      </PortletContent>
+    </Portlet>
+  );
+};
 
 export default withStyles(styles)(UsersTable);
--- webFront/src/views/User/component/Profile/ContentComponent/Itams/index.js
+++ webFront/src/views/User/component/Profile/ContentComponent/Itams/index.js
@@ -1,2 +1,3 @@
 export { default as BoardList } from './BoardList';
 export { default as BlockList } from './BlockList';
+export { default as Info } from './Info';
